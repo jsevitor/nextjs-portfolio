904b5d79536395100376cd92dfb05107e0640d4c
jsevitor <vitorjseo@gmail.com>
2025-08-14 13:00:43 -0300

    style(about): aplica novo design e paleta de cores

--- src/app/components/common/Buttons.tsx
+++ src/app/components/common/Buttons.tsx
@@ -2,19 +2,33 @@ type ButtonProps = {
   url?: string;
   label?: string;
   className?: string;
+  children?: React.ReactNode;
 };
 
 export function ButtonTalkToMe({ className }: ButtonProps) {
   return (
     <button
       type="submit"
-      className={`px-8 py-2 rounded-full bg-accent-green border border-dark transition-all duration-300 hover:scale-102 ${className}`}
+      className={`px-8 py-2 rounded-full bg-accent-green border border-dark transition-all duration-300 hover:scale-105 ${className}`}
     >
       <a
         href={"https://www.linkedin.com/in/josevitoroliveira/"}
         target="_blank"
       >
         Fale comigo
+      </a>
+    </button>
+  );
+}
+
+export function ButtonGreen({ url, children, className }: ButtonProps) {
+  return (
+    <button
+      type="submit"
+      className={`px-8 py-2 bg-accent-green text-dark rounded-full border border-dark transition-all duration-300 hover:scale-105 ${className}`}
+    >
+      <a href={url} target="_blank">
+        {children}
       </a>
     </button>
   );
--- src/app/components/sections/About.tsx
+++ src/app/components/sections/About.tsx
@@ -7,6 +7,7 @@ import {
   StacksSkeleton,
 } from "@/app/components/feedback/Skeletons";
 import API_URL from "@/lib/apiConfig";
+import { ButtonGreen } from "../common/Buttons";
 
 interface About {
   id: string;
@@ -59,65 +60,74 @@ export default function About() {
   }, []);
 
   return (
-    <div className="container mx-auto px-4 md:px-0 pt-[88px]" id="about">
-      <div className="flex flex-col gap-8 py-8">
-        <div className="border-l-2 border-foreground pl-8">
-          <h1 className="text-4xl sm:text-5xl lg:text-6xl font-[100] py-2">
-            SOBRE
-          </h1>
-        </div>
-        {loading ? (
-          <AboutSkeleton />
-        ) : (
-          aboutData.map((item) => (
-            <div
-              key={item.id}
-              className="flex flex-col lg:flex-row justify-center gap-8 my-16"
-            >
-              <div className="shrink-0 flex justify-center">
-                <Image
-                  src={item.image}
-                  alt="Foto de perfil"
-                  width={400}
-                  height={400}
-                  className="border border-foreground"
-                />
-              </div>
-              <div className="flex flex-col justify-center gap-4 p-2 lg:ml-8 lg:w-1/2">
-                <div className="flex items-center gap-2">
-                  <i className="bi bi-geo-alt-fill"></i>
-                  <h3>{item.location}</h3>
-                </div>
-                <p className="text-lg/8 font-extralight ">{item.content}</p>
-                <div className="flex justify-center lg:justify-start">
-                  <a href={item.curriculum} target="_blank">
-                    <button className="flex items-center gap-2 font-medium bg-foreground text-background px-8 py-1 rounded cursor-pointer mt-8 hover:bg-gray-medium">
-                      Currículo
-                      <i className="bi bi-arrow-down-circle"></i>
-                    </button>
-                  </a>
-                </div>
-              </div>
-            </div>
-          ))
-        )}
-        <div className="felx flex-col items-center justify-center text-gray-medium">
-          <h3 className="text-lg  text-center mb-4">TECH STACKS</h3>
-
+    <section
+      className="border border-gray rounded-4xl py-[64px] md:mt-[88px] shadow bg-[#cacac8]"
+      id="about"
+    >
+      <div className="container mx-auto px-4 ">
+        <div className="flex flex-col gap-8">
+          <div>
+            <h1 className="text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+              SOBRE MIM
+            </h1>
+          </div>
           {loading ? (
-            <StacksSkeleton />
+            <AboutSkeleton />
           ) : (
-            <div className="flex justify-center flex-wrap gap-4 lg:justify-between border border-gray-medium py-4 px-8">
-              {stacks.map((item) => (
-                <div key={item.id} className="flex flex-col items-center gap-4">
-                  <i className={`${item.icon} text-6xl`}></i>
-                  <p>{item.name}</p>
+            aboutData.map((item) => (
+              <div
+                key={item.id}
+                className="flex flex-col lg:flex-row justify-center gap-8 pb-8 lg:py-8 border border-gray shadow rounded-2xl"
+              >
+                <div className="shrink-0 flex justify-center rounded-2xl shadow">
+                  <Image
+                    src={item.image}
+                    alt="Foto de perfil"
+                    width={420}
+                    height={400}
+                    className="border border-gray rounded-2xl"
+                  />
                 </div>
-              ))}
-            </div>
+                <div className="flex flex-col justify-center gap-4 p-2 lg:ml-8 lg:w-1/2">
+                  <div className="flex items-center gap-2">
+                    <i className="bi bi-geo-alt-fill"></i>
+                    <h3>{item.location}</h3>
+                  </div>
+                  <p className="text-lg/8 ">{item.content}</p>
+                  <div className="flex justify-center lg:justify-start">
+                    <ButtonGreen url={item.curriculum} className="shadow mt-4">
+                      <span className="flex items-center gap-2">
+                        <p className="font-medium">Currículo</p>
+                        <i className="bi bi-arrow-down-circle"></i>
+                      </span>
+                    </ButtonGreen>
+                  </div>
+                </div>
+              </div>
+            ))
           )}
+          <div className="felx flex-col items-center justify-center text-dark mt-8">
+            <h3 className="text-xl text-center font-extrabold mb-4">
+              HABILIDADES TÉCNICAS
+            </h3>
+            {loading ? (
+              <StacksSkeleton />
+            ) : (
+              <div className="grid grid-cols-3 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 gap-2 lg:gap-4 text-sm   border-t border-b border-gray py-4 lg:px-8">
+                {stacks.map((item) => (
+                  <div
+                    key={item.id}
+                    className="flex flex-col items-center gap-2 p-4 rounded-xl bg-gray shadow"
+                  >
+                    <i className={`${item.icon} text-4xl lg:text-6xl`}></i>
+                    <p>{item.name}</p>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
