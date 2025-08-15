37ef3449a204ba8891f3b164c563cc16860a54d4
jsevitor <vitorjseo@gmail.com>
2025-08-14 22:24:46 -0300

    style(footer): aplica novo design e paleta de cores

--- src/app/components/layout/Footer.tsx
+++ src/app/components/layout/Footer.tsx
@@ -1,21 +1,21 @@
 export default function Footer() {
   return (
-    <div className="container mx-auto px-4 my-4 md:px-0">
-      <div className="flex flex-col justify-center items-center text-gray-medium text-xs">
-        <p className="text-center">© 2025 PORTFÓLIO.</p>
+    <footer className="bg-dark rounded-t-4xl h-28 flex items-center">
+      <div className="container mx-auto px-4 my-4 md:px-0 flex flex-col justify-center text-gray text-xs">
+        <p className="text-center text-accent-green">© 2025 PORTFÓLIO.</p>
         <p className="text-center">Todos os direitos reservados.</p>
         <p className="text-center">
           Desenvolvido por{" "}
           <a
             href="https://github.com/jsevitor"
             target="_blank"
-            className="underline text-foreground"
+            className="underline text-accent-green"
           >
             Vitor Oliveira
           </a>
           .
         </p>
       </div>
-    </div>
+    </footer>
   );
 }
